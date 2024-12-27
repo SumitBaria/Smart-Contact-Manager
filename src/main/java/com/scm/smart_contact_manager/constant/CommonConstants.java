a3cff37977fbd0c5c36f648e14200b4cfe059b13
Sumit Baria <imsumitbaria@gmail.com>
2024-12-27 23:08:03 +0530

    [Master] - Initial Commit

---
+++ src/main/java/com/scm/smart_contact_manager/constant/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.scm.smart_contact_manager.constant;
+
+public class CommonConstants {
+
+    private CommonConstants(){}
+
+    public static final String CONTACT_NOT_FOUND = "Contact Not Found";
+}
